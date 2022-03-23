985cd9a136f008a91fabe49170bcc717714b7f0b
Ceyck <angel_ab86@outlook.com>
2022-03-22 21:22:54 -0600

    LazyLoad listo

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,61 +1,46 @@
+import { Suspense } from "react";
 import {
-	BrowserRouter,
+	BrowserRouter as Router,
 	Switch,
 	Route,
 	NavLink,
 	Redirect,
 } from "react-router-dom";
 import logo from "../logo.svg";
-
-import { LazyPage1, LazyPage2, LazyPage3 } from "../01-lazyload/pages";
+import routes from "./routes";
 
 export const Navigation = () => {
 	return (
-		<BrowserRouter>
-			<div className="main-layout">
-				<nav>
-					<img src={logo} alt="react-logo" />
-					<ul>
-						<li>
-							<NavLink
-								to="/lazy1"
-								activeClassName="nav-active"
-							>
-								Lazy1
-							</NavLink>
-						</li>
-						<li>
-							<NavLink
-								to="/lazy2"
-								activeClassName="nav-active"
-							>
-								Lazy2
-							</NavLink>
-						</li>
-						<li>
-							<NavLink
-								to="/lazy3"
-								activeClassName="nav-active"
-							>
-								Lazy3
-							</NavLink>
-						</li>
-					</ul>
-				</nav>
+		<Suspense fallback={<span>Loading...</span>}>
+			<Router>
+				<div className="main-layout">
+					<nav>
+						<img src={logo} alt="react-logo" />
+						<ul>
+							{routes.map(({ name, path }) => (
+								<li key={path}>
+									<NavLink
+										to={path}
+										activeClassName="nav-active"
+										exact
+									>
+										{name}
+									</NavLink>
+								</li>
+							))}
+						</ul>
+					</nav>
 
-				<Switch>
-					<Route path="/lazy1" exact>
-						<LazyPage1 />
-					</Route>
-					<Route path="/lazy2" exact>
-						<LazyPage2 />
-					</Route>
-					<Route path="/lazy3" exact>
-						<LazyPage3 />
-					</Route>
-					<Redirect to="/lazy1" />
-				</Switch>
-			</div>
-		</BrowserRouter>
+					<Switch>
+						{routes.map(({ path, Component }) => (
+							<Route key={path} path={path} render={(props) => {
+								return <Component />
+							}} />
+						))}
+						<Redirect to={routes[0].path} />
+					</Switch>
+				</div>
+			</Router>
+		</Suspense>
 	);
 };
